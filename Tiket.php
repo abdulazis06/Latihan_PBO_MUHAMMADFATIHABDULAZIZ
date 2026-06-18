a627441c1fec0bc8bb3159b885a01feb79f57d71
abdulazis06 <muhammadfatihahi4@gmail.com>
2026-06-18 12:20:52 +0700

    [Tahap 3] Membuat abstract class Tiket beserta metode abstrak dengan PHP

---
+++ Tiket.php
@@ -0,0 +1,28 @@
+<?php
+abstract class Tiket {
+    // Properti Terenkapsulasi (protected)
+    protected $id_tiket;
+    protected $nama_film;
+    protected $jadwal_tayang;
+    protected $jumlah_kursi;
+    protected $harga_dasar_tiket;
+
+    // Constructor di PHP menggunakan __construct
+    public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $harga_dasar_tiket) {
+        $this->id_tiket = $id_tiket;
+        $this->nama_film = $nama_film;
+        $this->jadwal_tayang = $jadwal_tayang;
+        $this->jumlah_kursi = $jumlah_kursi;
+        $this->harga_dasar_tiket = $harga_dasar_tiket;
+    }
+
+    // Metode Abstrak
+    abstract public function hitungTotalHarga();
+    abstract public function tampilkanInfoFasilitas();
+
+    // Getter untuk dipanggil di Tahap 6 (View) nanti
+    public function getNamaFilm() { return $this->nama_film; }
+    public function getJadwalTayang() { return $this->jadwal_tayang; }
+    public function getJenisStudio() { return static::class; } // Mengambil nama class anak
+}
+?>
